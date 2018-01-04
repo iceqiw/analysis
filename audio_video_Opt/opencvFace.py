cdcb64c7df96508b9f453446fa3b1b5dcd476c50
iceqiw <qqwei1123@163.com>
2018-01-04 18:05:57 +0800

    opencv

--- audio_video_Opt/opencvFace.py
+++ audio_video_Opt/opencvFace.py
@@ -2,6 +2,26 @@ import cv2
 import numpy as np
 
 
+def doSomething():
+    print("make make made")
+
+
+def catchFace(frame,classifier):
+    size = frame.shape[:2]
+    image = np.zeros(size, dtype=np.float16)
+    image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    cv2.equalizeHist(image, image)
+    divisor = 8
+    h, w = size
+    minSize = (int(w / divisor), int(h / divisor))
+    faceRects = classifier.detectMultiScale(
+        image,
+        scaleFactor=1.2,
+        minNeighbors=2,
+        flags=cv2.CASCADE_SCALE_IMAGE,
+        minSize=minSize)
+    return faceRects
+
 def main():
     cv2.namedWindow("test")
     cap = cv2.VideoCapture("rtsp://192.168.14.139:8080/1122")
@@ -11,26 +31,15 @@ def main():
         success, frame = cap.read()
         if not success:
             break
-
-        size = frame.shape[:2]
-        image = np.zeros(size, dtype=np.float16)
-        image = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        cv2.equalizeHist(image, image)
-        divisor = 8
-        h, w = size
-        minSize = (int(w / divisor), int(h / divisor))
-        faceRects = classifier.detectMultiScale(
-            image,
-            scaleFactor=1.2,
-            minNeighbors=2,
-            flags=cv2.CASCADE_SCALE_IMAGE,
-            minSize=minSize)
-
+        faceRects =catchFace(frame,classifier)
         if len(faceRects) > 0:
-
             for faceRect in faceRects:
                 x, y, w, h = faceRect
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0))
+            doSomething()
+        else:
+            print("no face")    
+            
         cv2.imshow("test", frame)
         key = cv2.waitKey(10)
         c = chr(key & 255)
